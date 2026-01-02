cbdd3a50a43da5e42592be2b2e5b70726a03ae28
sanmipaul <sanmipaul1@gmail.com>
2026-01-02 22:11:28 +0100

    docs: enhance Ownable contract documentation

--- src/Ownable.sol
+++ src/Ownable.sol
@@ -2,7 +2,16 @@ pragma solidity ^0.8.19;
 
 /**
  * @title Ownable
- * @dev Contract module which provides a basic access control mechanism
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
  */
 abstract contract Ownable {
     address private _owner;
