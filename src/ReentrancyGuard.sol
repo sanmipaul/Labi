1754e0e33fb0e9d85a6dbc0aef807c24cb0bbb9c
sanmipaul <sanmipaul1@gmail.com>
2026-01-02 22:18:52 +0100

    docs: enhance ReentrancyGuard documentation and comments

--- src/ReentrancyGuard.sol
+++ src/ReentrancyGuard.sol
@@ -12,8 +12,23 @@ pragma solidity ^0.8.19;
  * `nonReentrant` may not call one another. This can be worked around by making
  * those functions `private`, and then adding `external` `nonReentrant` entry
  * points to them.
+ *
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
  */
 abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
     uint256 private constant NOT_ENTERED = 1;
     uint256 private constant ENTERED = 2;
 
